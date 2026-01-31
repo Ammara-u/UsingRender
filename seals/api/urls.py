49ad5c9af1c36b296ff2fe1b91932cc0cf8fb5ef
Ammara-u <ammaraubaid10@gmail.com>
2026-01-30 22:21:55 -0800

    d

--- seals/api/urls.py
+++ seals/api/urls.py
@@ -4,7 +4,7 @@ from .views import SealsViewSet, SaleViewSet
 # from .views import search_seals
 # Use this specific name 'seals_router' to fix the ImportError you had earlier
 seals_router = DefaultRouter()
-seals_router.register(r'seals', SealsViewSet, basename="seals")
+seals_router.register(r'seals', SealsViewSet)
 seals_router.register(r'sales', SaleViewSet) # This registers the /api/sales/ path
 
 urlpatterns = [
