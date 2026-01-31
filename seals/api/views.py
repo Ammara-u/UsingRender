b4bbaa9c5a42955032d2958b24ce14f42ef57542
Ammara-u <ammaraubaid10@gmail.com>
2026-01-30 21:35:15 -0800

    last

--- seals/api/views.py
+++ seals/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from ..models import Seals, Sale  # Must be 'Seals'
 from .serializers import SealsSerializer, SaleSerializer
+from django.http import JsonResponse
+from ..models import Inventory
 
 class sealsViewSet(viewsets.ModelViewSet):
     queryset = Seals.objects.all() 
@@ -10,4 +12,19 @@ class saleViewSet(viewsets.ModelViewSet):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
     
+def search_seals(request):
+    query = request.GET.get("q", "")
     
+    results = Inventory.objects.filter(name__icontains=query)
+
+    data = [
+        {
+            "id": item.id,
+            "title": item.name,
+            "category": item.category,
+            "route": f"/inventory/{item.id}",
+        }
+        for item in results
+    ]
+
+    return JsonResponse(data, safe=False)
