0a7a9d31f0059daf4f675575819695c33848190d
Ammara-u <ammaraubaid10@gmail.com>
2026-02-09 05:04:41 -0800

    hey end

--- seals/models.py
+++ seals/models.py
@@ -4,19 +4,20 @@ from django.db import models
 
 
 class Sale(models.Model):
-    sealName = models.CharField(max_length=200)  # Link to your inventory item
+    # sealName = models.CharField(max_length=200)  # Link to your inventory item
+    partCode = models.CharField(max_length=100, null=True, blank=True)
     quantity = models.IntegerField()
     sold_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_sold = models.DateTimeField(auto_now_add=True)
     
 # Create your models here.
 class Seals(models.Model):
-    nameOfSeal=models.CharField(max_length=200)
-    partCode=models.IntegerField()
+    # nameOfSeal=models.CharField(max_length=200)
+    partCode = models.CharField(max_length=100, null=True, blank=True)
     description=models.CharField(max_length=300)
     price=models.IntegerField()
     stock=models.IntegerField(default=0)
-    minStock = models.IntegerField(default=5)
+    minStock = models.IntegerField(default=500)
  
 # from django.utils import timezone
 # Sale.objects.create(
