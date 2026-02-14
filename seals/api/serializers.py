bcf7b8c238fd4985e7e458422a721d7b2f2feb2e
Ammara-u <ammaraubaid10@gmail.com>
2026-02-14 09:03:18 -0800

    d

--- seals/api/serializers.py
+++ seals/api/serializers.py
@@ -1,27 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import Seals
+
 class SealsSerializer(ModelSerializer):
     class Meta:
         model=Seals
         fields = ['id', 'partCode', 'description', 'price', 'stock', 'minStock']        
+
 from rest_framework import serializers
 from ..models import Seals, Sale
 
 class SaleSerializer(serializers.ModelSerializer):
-    # Change 'seal.nameOfSeal' to 'sealName' if that's your field name
-    partCode = serializers.ReadOnlyField(source='partCode') 
+    partCode = serializers.ReadOnlyField(source='seal.partCode')
     total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Sale
-        # 2. Inclusion (This is where the fix happens)
-        fields = [
-            'id',  
-            'partCode',  # <--- MUST be here!
-            'quantity', 
-            'sold_price', 
-            'date_sold', 
-            'total_price'
-        ]
+        fields = ['id', 'partCode', 'quantity', 'sold_price', 'date_sold', 'total_price']
+
     def get_total_price(self, obj):
         return obj.quantity * obj.sold_price
