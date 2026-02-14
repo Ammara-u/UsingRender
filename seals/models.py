7d8a6dc8b388ea885eb061f700d154d57844d6e6
Ammara-u <ammaraubaid10@gmail.com>
2026-02-14 08:29:56 -0800

    d

--- seals/models.py
+++ seals/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Sale(models.Model):
     # sealName = models.CharField(max_length=200)  # Link to your inventory item
-    partCode = models.CharField(max_length=100,null=True, blank=True)
+    partCode = models.CharField(max_length=100,null=True)
     quantity = models.IntegerField()
     sold_price = models.DecimalField(max_digits=10, decimal_places=2)
     date_sold = models.DateTimeField(auto_now_add=True)
